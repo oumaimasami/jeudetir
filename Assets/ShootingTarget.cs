46c56c77f70c243a4c0939cde11d1225fb2804a0
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2021-02-13 00:44:15 +0100

    final

---
+++ Assets/ShootingTarget.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class ShootingTarget : MonoBehaviour
+{
+   
+
+    public Text txtscore;
+
+    public void MajScore(int sc)
+    {
+        txtscore.text = sc.ToString();
+    }
+
+    RaycastHit hit;
+    [SerializeField]
+    GameObject prefabExplosion;
+
+    [SerializeField]
+    private int score = 0;
+
+    void Update()
+    {
+         Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
+          Ray ray = Camera.main.ScreenPointToRay(center);
+          if (Input.GetButtonDown("Fire1"))
+          {
+              if (Physics.Raycast(ray, out hit, Mathf.Infinity)) //Renvoie V si le rayon croise un collider
+              { Destroy(hit.collider.transform.gameObject);
+                  GameObject enemy = Instantiate(prefabExplosion, hit.transform.position, Quaternion.identity);
+                  Destroy(enemy, 4f);
+                score++;
+                GameObject.Find("Canvas").GetComponent<ShootingTarget>().MajScore(score);
+              } //Détruire l’ennemi
+      
+        }
+
+   
+    }
+    
+}
