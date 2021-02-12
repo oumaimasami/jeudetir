a9b23af6bdb4633b35bb28bca9a3895719054995
oumaimasami <61888499+oumaimasami@users.noreply.github.com>
2021-02-12 23:05:51 +0100

    commit

--- Assets/ShootingTarget.cs
+++ Assets/ShootingTarget.cs
@@ -2,33 +2,46 @@
 using UnityEngine.UI;
 public class ShootingTarget : MonoBehaviour
 {
- [SerializeField]
- Text txtScore;
- private int score = 0;
- public int Monscore {
-    get { return score; }
-    set { score = value; txtScore.text = "Score" + score; } }
- Monscore++;
+    /* [SerializeField]
+      Text txtScore;
+     private int score = 0;
+     public int Monscore {
+      get { return score; }
+      set { score = value; txtScore.text = "Score" + score; } }
+     */
+    
 
+    public Text txtscore;
+
+    public void MajScore(int sc)
+    {
+        txtscore.text = sc.ToString();
+    }
 
     RaycastHit hit;
     [SerializeField]
     GameObject prefabExplosion;
-   
-                  
-        
+
+    [SerializeField]
+    private int score = 0;
+
     void Update()
     {
-        Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
-        Ray ray = Camera.main.ScreenPointToRay(center);
-        if (Input.GetButtonDown("Fire1"))
-        {
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity)) //Renvoie V si le rayon croise un collider
-            { Destroy(hit.collider.transform.gameObject);
-                GameObject Go = Instantiate(prefabExplosion, hit.transform.position, Quaternion.identity);
-                Destroy(Go, 5f);
-            } // Détruire l’ennemi
+         Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
+          Ray ray = Camera.main.ScreenPointToRay(center);
+          if (Input.GetButtonDown("Fire1"))
+          {
+              if (Physics.Raycast(ray, out hit, Mathf.Infinity)) //Renvoie V si le rayon croise un collider
+              { Destroy(hit.collider.transform.gameObject);
+                  GameObject Go = Instantiate(prefabExplosion, hit.transform.position, Quaternion.identity);
+                  Destroy(Go, 4f);
+                score++;
+                GameObject.Find("Canvas").GetComponent<ShootingTarget>().MajScore(score);
+              } //Détruire l’ennemi
+      //      Monscore++;
         }
+
+   
     }
-     
+    
 }
