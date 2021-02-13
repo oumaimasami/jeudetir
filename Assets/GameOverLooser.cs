18158d631e2c594682cf411e71cf20ea8890813f
medayoubammar <45518536+medayoubammar@users.noreply.github.com>
2021-02-13 15:59:18 +0100

    v2

---
+++ Assets/GameOverLooser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverLooser: MonoBehaviour {
+ [SerializeField]
+ GameObject GameOver;
+ void OnCollisionEnter(Collision collision )
+ { GameOver.SetActive(true); }
+}
